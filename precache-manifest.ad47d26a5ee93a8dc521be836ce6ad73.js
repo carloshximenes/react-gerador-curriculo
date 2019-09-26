e954e2217e98a1cef21e1d902f127352e68b8ca0
CARLOS HENRIQUE XIMENES FIGUEIREDO MOTA <henriqueximenes@3coracoes.com.br>
2019-09-26 10:54:16 -0300

    Updates

---
+++ precache-manifest.ad47d26a5ee93a8dc521be836ce6ad73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43b222758c64f9aa4f0b8f29bd2bc30d",
+    "url": "/gerar-curriculo/index.html"
+  },
+  {
+    "revision": "a6518fc96de75c9e17a1",
+    "url": "/gerar-curriculo/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "e50eb6cae3fcc15f7506",
+    "url": "/gerar-curriculo/static/css/main.faf4fef6.chunk.css"
+  },
+  {
+    "revision": "a6518fc96de75c9e17a1",
+    "url": "/gerar-curriculo/static/js/2.3c85a63c.chunk.js"
+  },
+  {
+    "revision": "e50eb6cae3fcc15f7506",
+    "url": "/gerar-curriculo/static/js/main.dcea81c2.chunk.js"
+  },
+  {
+    "revision": "01b22f8f7c0a51a17f35",
+    "url": "/gerar-curriculo/static/js/runtime-main.5b49b33d.js"
+  }
+]);
